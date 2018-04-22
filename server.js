863567e458a0652d25f51865cb6ac9307737bb6b
Bharat Kashyap <bharatkashyap@outlook.com>
2018-04-22 17:36:23 +0530

    Deploying

---
+++ server.js
@@ -0,0 +1,12 @@
+
+var express = require('express'),
+    path = require('path'),
+    serveStatic = require('serve-static');
+
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+
+var port = process.env.PORT || 8080;
+app.listen(port);
+
+console.log("Server running at port "+ port + "!");
